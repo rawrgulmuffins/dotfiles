86088f38a3f225fd53ab2e297f0506c03761e9fd
Alex LordThorsen <alex.lordthorsen@esentire.com>
2021-01-06 00:20:18 -0800

    updating dotfiles

--- dotfiles/publish.py
+++ dotfiles/publish.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from pathlib import Path
 from os.path import expanduser
 
 
@@ -17,7 +18,12 @@ files_to_publish = [
 current_files = os.listdir(".")
 current_dir = os.getcwd()
 for source_name, dest_name in files_to_publish:
-   source = os.path.join(current_dir, source_name)
-   user_home = expanduser("~")
-   dest = os.path.join(user_home, "{}".format(dest_name))
-   shutil.copyfile(source, dest)
+    source = os.path.join(current_dir, source_name)
+    user_home = expanduser("~")
+    dest = Path(user_home) / dest_name
+    # If we're writing do a directory ensure it exists first
+    try:
+        os.makedirs(dest.parent, exist_ok=True)
+    except FileExistsError:
+        pass
+    shutil.copyfile(source, dest)
